41fc65daabe861f32301dc9a1790d3478dea6815
parth2104 <barotparth512@gmail.com>
2026-01-22 19:25:06 +0530

    Add configuration file to manage environment variables for API keys and models

---
+++ src/config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+class Config:
+    OPENAI_API_KEY=os.getenv("openai_api_key")
+    ENDPOINT=os.getenv("endpoint")
+    HF_TOKEN=os.getenv("HF_Token")
+    GROQ_API_KEY=os.getenv("Groq")
+    RAG_MODEL="qwen/qwen3-32b"
+    EMBEDDING_MODEL="sentence-transformers/all-MiniLM-L6-v2"
+    ASTRA_DB_TOKEN=os.getenv("Astra_db_Token")
+    ASTRA_DB_ENDPOINT=os.getenv("Astra_db_End_Point")
+    ASTRA_DB_KEY_SPACE=os.getenv("Astra_db_Keyspace")
