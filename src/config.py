e68980bd3a78b06a55eace5da3199240648165a8
parth2104 <barotparth512@gmail.com>
2026-01-28 20:20:51 +0530

    Fix environment variable names in Config class and update RAG model

--- src/config.py
+++ src/config.py
@@ -8,8 +8,8 @@ class Config:
     OPENAI_API_KEY=os.getenv("openai_api_key")
     ENDPOINT=os.getenv("endpoint")
     HF_TOKEN=os.getenv("HF_Token")
-    GROQ_API_KEY=os.getenv("Groq")
-    RAG_MODEL="qwen/qwen3-32b"
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    RAG_MODEL="llama-3.3-70b-versatile"
     EMBEDDING_MODEL="sentence-transformers/all-MiniLM-L6-v2"
     ASTRA_DB_TOKEN=os.getenv("Astra_db_Token")
     ASTRA_DB_ENDPOINT=os.getenv("Astra_db_End_Point")
